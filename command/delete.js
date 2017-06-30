4ae8c4942828d125b0904db88d61a52efe1fa165
goze <goze.qiu@gmail.com>
2017-06-30 15:25:37 +0800

    fix: update method --> delete key

--- command/delete.js
+++ command/delete.js
@@ -1,7 +1,7 @@
 'use strict'
 const co = require('co')
 const prompt = require('co-prompt')
-const config = require('../templates')
+const templates = require('../templates')
 const chalk = require('chalk')
 const fs = require('fs')
 const emoji = require('node-emoji')
@@ -11,18 +11,18 @@ module.exports = () => {
   co(function* () {
     let tplName = yield prompt('Template name: ')
 
-    if (config.tpl[tplName]) {
-      config.tpl[tplName] = undefined
+    if (templates.tpl[tplName]) {
+      delete templates.tpl[tplName]
     } else {
       console.log(`${emoji.get(':warning:')}' ${chalk.red('Template does not exist!')}`)
       process.exit()
     }
 
-    fs.writeFile(__dirname + '/../templates.json', JSON.stringify(config), 'utf-8', (err) => {
+    fs.writeFile(__dirname + '/../templates.json', JSON.stringify(templates), 'utf-8', (err) => {
       if (err) console.log(err)
       console.log(`${emoji.get(':sparkles:')}' ${chalk.green('Template deleted!')}`)
       console.log(chalk.grey('The last template list is: \n'))
-      utils.consoleTemplate();
+      utils.consoleTemplate(templates.tpl)
       console.log('\n')
       process.exit()
     })
