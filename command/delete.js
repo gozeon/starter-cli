3bfbed733279baa9c65e27c60104e2da103ef921
goze <goze.qiu@gmail.com>
2017-07-14 10:59:48 +0800

    feat: use ora

--- command/delete.js
+++ command/delete.js
@@ -6,27 +6,34 @@ const chalk = require('chalk')
 const fs = require('fs')
 const emoji = require('node-emoji')
 const utils = require('../utils')
+const Ora = require('ora')
 
 module.exports = () => {
   co(function* () {
     let tplName = yield prompt('Template name: ')
+    const spinner = new Ora();
 
-    if (templates.tpl[tplName]) {
-      delete templates.tpl[tplName]
-    } else {
-      utils.showNotifier('Warning', `Template ${tplName} does not exist!`)
-      console.log(`${emoji.get(':warning:')}  ${chalk.red('Template does not exist!')}`)
-      process.exit()
-    }
+    spinner.start(` template ${chalk.yellow(tplName)} is deleting.`);
 
-    fs.writeFile(__dirname + '/../templates.json', JSON.stringify(templates), 'utf-8', (err) => {
-      if (err) console.log(err)
-        utils.showNotifier('Success', `Template ${tplName} deleted!`)
-      console.log(`${emoji.get(':sparkles:')}  ${chalk.green('Template deleted!')}`)
-      console.log(chalk.grey('The last template list is: \n'))
-      utils.consoleTemplate(templates.tpl)
-      console.log('\n')
-      process.exit()
-    })
+    setTimeout(() => {
+
+      if (templates.tpl[tplName]) {
+        delete templates.tpl[tplName]
+      } else {
+        utils.showNotifier('Warning', `Template ${tplName} does not exist!`)
+        spinner.warn(` ${chalk.yellow(tplName)} has already existed!`)
+        process.exit()
+      }
+
+      fs.writeFile(__dirname + '/../templates.json', JSON.stringify(templates), 'utf-8', (err) => {
+        if (err) console.log(err)
+        utils.showNotifier('Success', `Template ${tplName} is deleted!`)
+        spinner.succeed(` ${chalk.yellow(tplName)} is deleted!`)
+        console.log(chalk.grey('The last template list is: \n'))
+        utils.consoleTemplate(templates.tpl)
+        console.log('\n')
+        process.exit()
+      })
+    }, 1800)
   })
 }
