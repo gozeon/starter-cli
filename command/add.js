00e3b1b0668ea56ef4639c9fef798efbbd3f77fb
goze <goze.qiu@gmail.com>
2017-07-14 10:47:24 +0800

    feat: add ora

--- command/add.js
+++ command/add.js
@@ -6,31 +6,38 @@ const chalk = require('chalk')
 const fs = require('fs')
 const emoji = require('node-emoji')
 const utils = require('../utils');
+const Ora = require('ora');
 
 module.exports = () => {
   co(function* () {
     let tplName = yield prompt('Template name: ')
     let gitUrl = yield prompt('Git https link: ')
     let branch = yield prompt('Branch: ')
+    const spinner = new Ora();
+
+    spinner.start(` template ${chalk.yellow(tplName)} is adding.`);
+
+    setTimeout(() => {
 
     if (!templates.tpl[tplName]) {
       templates.tpl[tplName] = {}
       templates.tpl[tplName]['url'] = gitUrl.replace(/[\u0000-\u0019]/g, '')
       templates.tpl[tplName]['branch'] = branch
     } else {
-      utils.showNotifier('Warning', 'Template has already existed!')
-      console.log(`${emoji.get(':warning:')}  ${chalk.red('Template has already existed!')}`)
+      spinner.warn(` ${tplName} has already existed!`)
+      utils.showNotifier('Warning', `${tplName} has already existed!`)
       process.exit()
     }
 
     fs.writeFile(__dirname + '/../templates.json', JSON.stringify(templates), 'utf-8', (err) => {
       if (err) console.log(err)
+      spinner.succeed(` New template ${tplName} is added!`)
       utils.showNotifier('Success', `New template ${tplName} added!`)
-      console.log(`${emoji.get(':sparkles:')}  ${chalk.green('New template added!\n')}`)
-      console.log(chalk.grey('The last template list is: \n'))
+      console.log(chalk.grey('The latest template list is: \n'))
       utils.consoleTemplate(templates.tpl)
       console.log('\n')
       process.exit()
     })
+    }, 1800)
   })
 }
